d3a999cf2f127b0fefeec38ae9a9e58c72178e12
mierzvoj <mierzvoj@gmail.com>
2021-05-09 15:49:29 +0200

    Third commit

--- movieservice/src/main/java/com/example/pjatk/movieservice/service/MovieService.java
+++ movieservice/src/main/java/com/example/pjatk/movieservice/service/MovieService.java
@@ -3,22 +3,16 @@ package com.example.pjatk.movieservice.service;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class MovieService {
 
-    Movie movie;
-
-    public List<Movie> movies = new ArrayList<>();
-
-    public MovieService(Movie movie, List<Movie> movies) {
-        this.movie = movie;
-        this.movies = movies;
-    }
+    List<Movie> movieList = Arrays.asList(new Movie(1L, "mv1", "scary"), new Movie(2L, "mv2", "scary"));
 
     public List<Movie> listMovies() {
-        return movies;
+        return movieList;
     }
 
 
